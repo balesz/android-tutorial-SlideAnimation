25e2266f4721c163d0c0b9cdac9b851339690e9c
Bálint Böde <balint.bode@gmail.com>
2013-09-11 22:25:01 +0200

    The tutorial is working

--- SlideAnimation/src/main/java/net/solutinno/tutorial/slide/MainActivity.java
+++ SlideAnimation/src/main/java/net/solutinno/tutorial/slide/MainActivity.java
@@ -1,27 +1,31 @@
 package net.solutinno.tutorial.slide;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
-import android.support.v4.view.MarginLayoutParamsCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.LayoutAnimationController;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.TranslateAnimation;
-import android.widget.ScrollView;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.nineoldandroids.view.ViewPropertyAnimator;
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
     private TextView mTextView;
-    private ScrollView mScrollView;
+    private ListView mListView;
 
     private GestureDetectorCompat mGesture;
 
@@ -29,14 +33,41 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = (TextView) findViewById(R.id.textview);
-        mScrollView = (ScrollView) findViewById(R.id.scrollview);
+
         mGesture = new GestureDetectorCompat(this, mOnGestureListener);
-        mScrollView.setOnTouchListener(mOnTouchListener);
+
+        mTextView = (TextView) findViewById(R.id.textview);
+        mListView = (ListView) findViewById(R.id.listview);
+
+        initListView();
     }
 
-    void log(String format, Object... args) {
-        Log.d("SOLUTINNO", String.format(format, args));
+    void initListView() {
+        mTextView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+
+        View header = new View(this);
+        header.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, mTextView.getMeasuredHeight()));
+
+        mListView.addHeaderView(header, null, false);
+        mListView.setHeaderDividersEnabled(true);
+        mListView.setOnTouchListener(mOnTouchListener);
+
+        ArrayList<String> data = new ArrayList<String>();
+        for (int i = 1; i <= 100; ++i) data.add(String.format("Test Data %d", i));
+        mListView.setAdapter(new ColorArrayAdapter(this, android.R.layout.simple_list_item_1, data));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_reset) {
+            resetListViewAndBar();
+        }
+        return true;
     }
 
     View.OnTouchListener mOnTouchListener = new View.OnTouchListener() {
@@ -49,55 +80,65 @@ public class MainActivity extends ActionBarActivity {
 
     GestureDetector.OnGestureListener mOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
         @Override
-        public boolean onSingleTapConfirmed(MotionEvent e) {
-            mTextView.setTranslationY(0);
-            textViewAnimator(0);
-            return true;
-        }
-        @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            textViewPropertyAnimation(distanceY);
-            //textViewAnimator(distanceY);
-            return true;
+            topBarAnimation(distanceY);
+            return super.onScroll(e1, e2, distanceX, distanceY);
         }
     };
 
-    private void textViewAnimator(float distanceY) {
-        if (mTextView.getVisibility() == View.GONE) {
-            TranslateAnimation anim = new TranslateAnimation(0,0,-mTextView.getHeight(),0);
-            anim.setDuration(200);
-            anim.setFillAfter(true);
-            anim.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) { }
-                @Override
-                public void onAnimationEnd(Animation animation) { mTextView.setVisibility(View.VISIBLE); }
-                @Override
-                public void onAnimationRepeat(Animation animation) { }
-            });
-            mTextView.startAnimation(anim);
+    float mTranslation;
+
+    void topBarAnimation(float distanceY) {
+        mTranslation += (-distanceY);
+        if (distanceY > 0) mTranslation = mTranslation < -mTextView.getHeight() ? -mTextView.getHeight() : mTranslation;
+        else if (distanceY < 0) mTranslation = mTranslation > 0 ? 0 : mTranslation;
+
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(0);
+        set.playTogether(
+            ObjectAnimator.ofInt(mTextView, "top", (int)mTranslation).setDuration(0),
+            ObjectAnimator.ofInt(mTextView, "bottom", mTextView.getHeight()+(int)mTranslation).setDuration(0)
+        );
+        set.start();
+    }
+
+    void resetListViewAndBar() {
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(0);
+        set.addListener(new AnimationListenerImp() {
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                mTranslation = 0;
+                mListView.setSelection(0);
+            }
+        });
+        set.playTogether(
+            ObjectAnimator.ofInt(mTextView, "top", 0).setDuration(0),
+            ObjectAnimator.ofInt(mTextView, "bottom", mTextView.getHeight()).setDuration(0)
+        );
+        set.start();
+    }
+
+    public abstract class AnimationListenerImp implements Animator.AnimatorListener {
+        public void onAnimationStart(Animator animator) {}
+        public void onAnimationEnd(Animator animator) {}
+        public void onAnimationCancel(Animator animator) {}
+        public void onAnimationRepeat(Animator animator) {}
+    }
+
+    public static class ColorArrayAdapter extends ArrayAdapter<String> {
+        public ColorArrayAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
         }
-        else {
-            TranslateAnimation anim = new TranslateAnimation(0,0,0,-mTextView.getHeight());
-            anim.setDuration(200);
-            anim.setFillAfter(true);
-            anim.setAnimationListener(new Animation.AnimationListener() {
-                @Override
-                public void onAnimationStart(Animation animation) { mTextView.setVisibility(View.GONE); }
-                @Override
-                public void onAnimationEnd(Animation animation) { }
-                @Override
-                public void onAnimationRepeat(Animation animation) { }
-            });
-            mTextView.startAnimation(anim);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = super.getView(position, convertView, parent);
+            assert view != null;
+            if (position % 2 == 0) view.setBackgroundResource(R.color.blue);
+            else if (position % 3 == 0) view.setBackgroundResource(R.color.green);
+            else view.setBackgroundResource(R.color.red);
+            return view;
         }
     }
 
-    float textViewTranslation;
-    void textViewPropertyAnimation(float distanceY) {
-        textViewTranslation += (-distanceY);
-        if (distanceY > 0) textViewTranslation = textViewTranslation < -mTextView.getHeight() ? -mTextView.getHeight() : textViewTranslation;
-        else if (distanceY < 0) textViewTranslation = textViewTranslation > 0 ? 0 : textViewTranslation;
-        ViewPropertyAnimator.animate(mTextView).setDuration(0).translationY(textViewTranslation);
-    }
 }
